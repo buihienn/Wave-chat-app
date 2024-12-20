17330609548bec94ad1ddd7ae6775c3889fd4490
DinhHoHo <hpv25072004@gmail.com>
2024-12-20 22:34:59 +0700

    add main class

--- WaveChatApp/src/main/java/com/wavechat/WaveChatApp.java
+++ WaveChatApp/src/main/java/com/wavechat/WaveChatApp.java
@@ -1,13 +1,29 @@
 package com.wavechat;
 import com.wavechat.form.*;
+import com.wavechat.socket.ClientSocketManager;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 
 public class WaveChatApp {
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            // Tạo và hiển thị frame
-            UserHomeMain frame = new UserHomeMain();
-            frame.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            try {
+                String serverAddress = "localhost";
+                int port = 1234;
+
+                ClientSocketManager clientSocket = new ClientSocketManager(serverAddress, port);
+//                GlobalVariable.setUserID("U008");
+//                UserHomeMain frame = new UserHomeMain(clientSocket);
+                AuthenticationMain frame = new AuthenticationMain(clientSocket);
+
+                frame.setVisible(true);
+                System.out.println("Connected to server at " + serverAddress + ":" + port);
+            } catch (IOException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Could not connect to server. Please check the server status.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
         });
     }
 }
